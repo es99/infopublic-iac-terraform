94ded61ea5773de9969fa20fdde5e316f0afe291
Engels Souza <engels.franca@icloud.com>
2024-08-26 11:58:43 -0300

    retirando o backend e deixando apenas no root module

--- modules/dev/ec2/provider.tf
+++ modules/dev/ec2/provider.tf
@@ -5,11 +5,5 @@ terraform {
       version = "~> 5.0"
     }
   }
-  backend "s3" {
-    bucket = "infopublic-iac-terraform-testes"
-    key    = "modules-iac-infopublic"
-    region = "us-east-2"
-  }
-
   required_version = "~> 1.5"
 }
