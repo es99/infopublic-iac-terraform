cd2ecfa5b2bf3185369f9ecbf55f07a812edbe01
Engels Souza <engels.franca@icloud.com>
2024-08-28 16:46:29 -0300

    Incluido mais outputs: id, public dns, public ip

--- modules/dev/ec2/main.tf
+++ modules/dev/ec2/main.tf
@@ -3,14 +3,14 @@ resource "aws_instance" "ec2" {
   instance_type               = var.instance_type
   associate_public_ip_address = true
   disable_api_termination     = true         //(Optional, if true, enables ec2 instance termination protection.)
-  iam_instance_profile        = var.iam_role //A role que a instancia irá utilizar
+  iam_instance_profile        = var.iam_role //(Optional) A role que a instancia irá utilizar
   security_groups             = var.security_groups
   subnet_id                   = var.subnet_id
-  key_name                    = var.key_name
+  key_name                    = var.key_name //(Optional)
 
   tags = {
     Name          = var.instance_name
-    Environment   = var.environment
+    Environment   = var.environment //(Optional)
     Owner         = var.owner
     MalwareDetect = "True"
   }
--- modules/dev/ec2/outputs.tf
+++ modules/dev/ec2/outputs.tf
@@ -1,3 +1,15 @@
 output "instance_type" {
   value = aws_instance.ec2.instance_type
 }
+
+output "id" {
+  value = aws_instance.ec2.id
+}
+
+output "public_dns" {
+  value = aws_instance.ec2.public_dns
+}
+
+output "public_ip" {
+  value = aws_instance.ec2.public_ip
+}
